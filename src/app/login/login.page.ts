40e613f1c108f4527dc84b5253346a6f2040b935
antoijh <geimerlol253@gmail.com>
2026-03-05 10:24:38 -0600

    arreglos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonNavLink} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule]
+  imports: [IonicModule, FormsModule, RouterLink]
 })
 export class LoginPage {
 
